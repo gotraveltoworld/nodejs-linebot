93ee2613d2dfef787674d48b970752b85ecb7303
T.H. Lin <futureworkeratworld@gmail.com>
2017-02-26 17:04:51 +0800

    Update

--- server.js
+++ server.js
@@ -18,57 +18,46 @@ const app = require('express')();
 const server = require('http').Server(app);
 const url = require('url');
 const fixieUrl = url.parse(process.env.FIXIE_URL);
-
 const requestUrl = url.parse('https://linebot-nodejs.herokuapp.com/');
 
-app.get({
-    host: fixieUrl.hostname,
-    port: fixieUrl.port,
-    path: requestUrl.href,
-    headers: {
-      Host: requestUrl.host,
-      'Proxy-Authorization': `Basic ${new Buffer(fixieUrl.auth).toString('base64')}`,
-    }
-}, res => {
-    app.set('port', process.env.PORT || 8080);
-    app.get('/', function (req, res) {
-        res.send('hello');
-    });
-    var LINEBot = require('line-messaging');
-    var bot = LINEBot.create({
-        channelID: '1482947046',
-        channelSecret: 'df2786945e22c3b8963bdae324c53654',
-        channelToken: 'oXujCqRaebFfWD4MZEsu3pPdwoO3ASeZtchwx8lTS6CTFYrZyBHBm9jPKu74Spk81IVkAi6m395Ck0jE' +
-                'sJGeeZVCkl68rs2m+5EZER3RpRveCPb6pJsgIWRxl3u+ux/u3Q+JgExekKyrUFg1Xo8kjwdB04t89/1O' +
-                '/w1cDnyilFU='
-    }, server);
-    app.use(bot.webhook('/webhook'));
-    bot.on(LINEBot.Events.MESSAGE, function (replyToken, message) {
-        console.log('replyToken', replyToken);
-        if (message.isMessageType('text')) {
-            if (message.getText() === 'day') {
-                var text = new LINEBot.TextMessageBuilder('I\'m a super man!');
-                var sticker = new LINEBot.StickerMessageBuilder(1, 109);
-                bot.replyMultiMessage(replyToken, [text, sticker]).then(function (data) {
-                    console.log('res1', data);
-                }).catch(function (error) {
-                    console.log('error1', error);
-                });
-                console.log('res1', message.getText());
-            } else {
-                var text1 = new LINEBot.TextMessageBuilder('Hi');
-                var sticker1 = new LINEBot.StickerMessageBuilder(1, 109);
-                bot.replyMultiMessage(replyToken, [text1, sticker1]).then(function (data) {
-                    console.log('res2', data);
-                }).catch(function (error) {
-                    console.log('error2', error);
-                });
-                console.log('res2', message.getText());
-            }
+app.set('port', process.env.PORT || 8080);
+app.get('/', function (req, res) {
+    res.send('hello');
+});
+var LINEBot = require('line-messaging');
+var bot = LINEBot.create({
+    channelID: '1482947046',
+    channelSecret: 'df2786945e22c3b8963bdae324c53654',
+    channelToken: 'oXujCqRaebFfWD4MZEsu3pPdwoO3ASeZtchwx8lTS6CTFYrZyBHBm9jPKu74Spk81IVkAi6m395Ck0jE' +
+            'sJGeeZVCkl68rs2m+5EZER3RpRveCPb6pJsgIWRxl3u+ux/u3Q+JgExekKyrUFg1Xo8kjwdB04t89/1O' +
+            '/w1cDnyilFU='
+}, server);
+app.use(bot.webhook('/webhook'));
+bot.on(LINEBot.Events.MESSAGE, function (replyToken, message) {
+    console.log('replyToken', replyToken);
+    if (message.isMessageType('text')) {
+        if (message.getText() === 'day') {
+            var text = new LINEBot.TextMessageBuilder('I\'m a super man!');
+            var sticker = new LINEBot.StickerMessageBuilder(1, 109);
+            bot.replyMultiMessage(replyToken, [text, sticker]).then(function (data) {
+                console.log('res1', data);
+            }).catch(function (error) {
+                console.log('error1', error);
+            });
+            console.log('res1', message.getText());
         } else {
-            console.log('message.getText()', message.getText());
+            var text1 = new LINEBot.TextMessageBuilder('Hi');
+            var sticker1 = new LINEBot.StickerMessageBuilder(1, 109);
+            bot.replyMultiMessage(replyToken, [text1, sticker1]).then(function (data) {
+                console.log('res2', data);
+            }).catch(function (error) {
+                console.log('error2', error);
+            });
+            console.log('res2', message.getText());
         }
-    });
+    } else {
+        console.log('message.getText()', message.getText());
+    }
 });
 
 server.listen(app.set('port'));
